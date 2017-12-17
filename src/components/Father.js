71a1e02c221f5f219e4f5c3f7cdf9a5d09cfb307
George Filippakis <geo.filippakis@gmail.com>
2017-12-17 15:49:51 +0200

    React bootstrap

--- src/components/Father.js
+++ src/components/Father.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Father.css';
 import Child from './Child';
 
+import { Button } from 'react-bootstrap';
+
+
 export default class Father extends React.Component {
     render() {
         return (
@@ -9,6 +12,11 @@ export default class Father extends React.Component {
                 <h3>React Playground</h3>
                 <h4>Father Component</h4>
                 <Child />
+                <Button
+                    bsStyle="success"
+                >
+                    Klaniol!
+                </Button>
             </div>
         );
     }
